14d7fab66e6af27fe8cb9104421e5316ac7cde4d
DesperateFish <1251948326@qq.com>
2022-02-27 10:43:34 +0800

    修改了原理图

---
+++ src/blinker_user.h
@@ -0,0 +1,12 @@
+#ifndef __blinker_user__H__
+#define __blinker_user__H__
+
+#define BLINKER_PRINT Serial
+#define BLINKER_WIFI
+
+#include <Blinker.h>
+
+
+void blinker_init();
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
  //把远程下载下来的代码合并到本地仓库，远程的和本地的合并
  // git merge origin / master
 
+#include <blinker_user.h>
 #include <Arduino.h>
  //#include "HX711.h"
 #include "pump.h"
@@ -8,64 +9,21 @@
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
 
-    float Weight = 0; //称重值初始化
+ float Weight = 0; //称重值初始化
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
-
-#define BLINKER_PRINT Serial
-#define BLINKER_WIFI
-
-#include <Blinker.h>
-
-char auth[] = "66b44e42cba1";
-char ssid[] = "TP-LINK_2011";
-char pswd[] = "20112011";
-
-//char ssid[] = "9Pro";
-//char pswd[] = "13451812385";
-
-// 新建组件对象
-BlinkerButton Button1("btn-abc");
-BlinkerNumber Number1("num-abc");
-
-int counter = 0;
-
-// 按下按键即会执行该函数
-void button1_callback(const String &state)
-{
-    BLINKER_LOG("get button state: ", state);
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-}
-
-// 如果未绑定的组件被触发，则会执行其中内容
-void dataRead(const String &data)
-{
-    BLINKER_LOG("Blinker readString: ", data);
-    counter++;
-    Number1.print(counter);
-}
 
     void setup()
     {
         // 初始化串口
      	Serial.begin(9600);	
-	Serial.print("Welcome to use!\n");
-//  Init_Hx711();				//初始化HX711模块连接的IO设置
-//    Init_Relay();
-	mlx.begin();  
-    oled_init();
-        BLINKER_DEBUG.stream(Serial);
-        BLINKER_DEBUG.debugAll();
-
-        // 初始化有LED的IO
-        pinMode(LED_BUILTIN, OUTPUT);
-        digitalWrite(LED_BUILTIN, HIGH);
-        // 初始化blinker
-        Blinker.begin(auth, ssid, pswd);
-        Blinker.attachData(dataRead);
-
-        Button1.attach(button1_callback);
-
+	    Serial.print("Welcome to use!\n");
+        blinker_init();
+        //  Init_Hx711();				//初始化HX711模块连接的IO设置
+        //    Init_Relay();
+        mlx.begin();
+        oled_init();
+    
     
     }
 
@@ -86,5 +44,4 @@ void dataRead(const String &data)
     Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
     Serial.print("*F\tObject = "); Serial.print(mlx.readObjectTempF()); Serial.println("*F");
 	delay(2000);		
-
     }
